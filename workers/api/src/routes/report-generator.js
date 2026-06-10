39c7f41cdc3a11151ece143d521d99718bc5d36d
TeddyBahary <teddybahary@gmail.com>
2026-06-10 10:47:20 -0400

    Release fixed report helper picker

--- workers/api/src/routes/report-generator.js
+++ workers/api/src/routes/report-generator.js
@@ -18,9 +18,9 @@ import {
 } from '../lib/report-credits.js'
 
 const route = new Hono()
-const REPORT_HELPER_FILENAME = 'SkillCascadeReportHelper-release-20260608-standard-renderer-v2.zip'
+const REPORT_HELPER_FILENAME = 'SkillCascadeReportHelper-release-20260610-folder-picker-fix.zip'
 const REPORT_HELPER_OBJECT_KEY = `report-generator/${REPORT_HELPER_FILENAME}`
-const REPORT_HELPER_VERSION = 'release-20260608-standard-renderer-v2'
+const REPORT_HELPER_VERSION = 'release-20260610-folder-picker-fix'
 const STANDARD_REPORT_TEMPLATE = {
   id: 'skillcascade-standard-initial-assessment-v1',
   label: 'SkillCascade Standard Initial Assessment',
